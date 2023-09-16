acca86b223c2db429427c01913d4b062a834943a
thongvlog27 <20t1020574@husc.edu.vn>
2023-09-16 08:39:34 +0700

    thongvlog27

--- thongtv/baitap1.cpp
+++ thongtv/baitap1.cpp
@@ -4,15 +4,15 @@
 int main(int argc, char const *argv[])
 {
   printf("HELLO C++\n");
-  LOG_D("WELCOME C++. VVDUNG\n");
-  LOG_I("WELCOME C++. VVDUNG\n");
-  LOG_W("WELCOME C++. VVDUNG\n");
-  LOG_E("WELCOME C++. VVDUNG\n");
+  LOG_D("WELCOME C++. THONGTV\n");
+  LOG_I("WELCOME C++. THONGTV\n");
+  LOG_W("WELCOME C++. THONGTV\n");
+  LOG_E("WELCOME C++. THONGTV\n");
 
-  LOG_DT("WELCOME C++. VVDUNG\n");
-  LOG_IT("WELCOME C++. VVDUNG\n");
-  LOG_WT("WELCOME C++. VVDUNG\n");
-  LOG_ET("WELCOME C++. VVDUNG\n");
+  LOG_DT("WELCOME C++. THONGTV\n");
+  LOG_IT("WELCOME C++. THONGTV\n");
+  LOG_WT("WELCOME C++. THONGTV\n");
+  LOG_ET("WELCOME C++. THONGTV\n");
 
   LOG_D("");
   return 0;
