0e57c663bc66f99c25b7876d40a4f09b069ac5d4
lequangkhenh <lequangkhanhltqb@gmail.com>
2023-09-16 08:15:25 +0700

    khanhlq

---
+++ khanhlq/baitap1.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "..\_COMMON\Log.h"
+
+int main(int argc, char const *argv[])
+{
+  LOG_D("WELCOME C++. khanhlq\n");
+  LOG_I("WELCOME C++. khanhlq\n");
+  LOG_W("WELCOME C++. khanhlq\n");
+  LOG_E("WELCOME C++. khanhlq\n");
+
+  LOG_DT("WELCOME C++. khanhlq\n");
+  LOG_IT("WELCOME C++. khanhlq\n");
+  LOG_WT("WELCOME C++. khanhlq\n");
+  LOG_ET("WELCOME C++. khanhlq\n");
+
+  LOG_D("");
+  return 0;
+}
