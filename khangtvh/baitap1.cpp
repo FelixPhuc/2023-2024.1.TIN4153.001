54c6e7574968a13899d659d65ae8bace90373c55
tvhkhang <84630541+tvhkhang@users.noreply.github.com>
2023-09-16 07:52:34 +0700

    Khangtvh

---
+++ khangtvh/baitap1.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "..\_COMMON\Log.h"
+
+int main(int argc, char const *argv[])
+{
+  LOG_D("WELCOME C++. tvhkhang\n");
+  LOG_I("WELCOME C++. tvhkhang\n");
+  LOG_W("WELCOME C++. tvhkhang\n");
+  LOG_E("WELCOME C++. tvhkhang\n");
+
+  LOG_DT("WELCOME C++. tvhkhang\n");
+  LOG_IT("WELCOME C++. tvhkhang\n");
+  LOG_WT("WELCOME C++. tvhkhang\n");
+  LOG_ET("WELCOME C++. tvhkhang\n");
+
+  LOG_D("");
+  return 0;
+}
