7bb8419962ae40436a4f6a837d53fb8f806419a7
NguyenHoangTien9910 <nhinhi171002@gmail.com>
2023-09-18 08:09:41 +0700

    tien

--- TienTien/baitap1.cpp
+++ TienTien/baitap1.cpp
@@ -3,15 +3,16 @@
 
 int main(int argc, char const *argv[])
 {
-  LOG_D("WELCOME C++. khanhlq\n");
-  LOG_I("WELCOME C++. khanhlq\n");
-  LOG_W("WELCOME C++. khanhlq\n");
-  LOG_E("WELCOME C++. khanhlq\n");
+  printf("HELLO C++\n");
+  LOG_D("WELCOME C++. VVDUNG\n");
+  LOG_I("WELCOME C++. VVDUNG\n");
+  LOG_W("WELCOME C++. VVDUNG\n");
+  LOG_E("WELCOME C++. VVDUNG\n");
 
-  LOG_DT("WELCOME C++. khanhlq\n");
-  LOG_IT("WELCOME C++. khanhlq\n");
-  LOG_WT("WELCOME C++. khanhlq\n");
-  LOG_ET("WELCOME C++. khanhlq\n");
+  LOG_DT("WELCOME C++. VVDUNG\n");
+  LOG_IT("WELCOME C++. VVDUNG\n");
+  LOG_WT("WELCOME C++. VVDUNG\n");
+  LOG_ET("WELCOME C++. VVDUNG\n");
 
   LOG_D("");
   return 0;
