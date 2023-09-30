420b560f4d3f5f2a1f4c36c633141f08692fec02
Trung2307 <inuyasa2020@gmail.com>
2023-09-30 08:58:15 +0700

    Trung2307

--- trungpnb/Trung_httpbat.bat
+++ trungpnb/Trung_httpbat.bat
@@ -2,10 +2,10 @@
 set CC=g++
 set SRC_COMMON=../_COMMON
 
-%CC% net_http.cpp ^
+%CC% Trung_http.cpp ^
      %SRC_COMMON%/Log.cpp ^
      %SRC_COMMON%/Utils.cpp ^
 -l ws2_32 ^
--o net_http.exe
+-o Trung_http.exe
 REM g++ main.cpp ../../_src/Log.cpp -o t.exe
-REM g++ .\net_http.cpp ..\_COMMON\Log.cpp -o net_http.exe
+REM g++ .\Trung_http.cpp ..\_COMMON\Log.cpp -o Trung_http.exe
