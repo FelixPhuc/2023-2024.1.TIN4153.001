065d5c66e9724fa040f7692047836188a12b3a42
Thanhha <20t1020157@husc.edu.vn>
2023-11-11 09:51:05 +0700

    cth

---
+++ cth/baitap1.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "..\_COMMON\Log.h"
+
+int main(int argc, char const *argv[])
+{
+  printf("HELLO C++\n");
+  LOG_D("WELCOME C++. THANH\n");
+  LOG_I("WELCOME C++. THANH\n");
+  LOG_W("WELCOME C++. THANH\n");
+  LOG_E("WELCOME C++. THANH\n");
+
+  LOG_DT("WELCOME C++. THANH\n");
+  LOG_IT("WELCOME C++. THANH\n");
+  LOG_WT("WELCOME C++. THANH\n");
+  LOG_ET("WELCOME C++. THANH\n");
+
+  LOG_D("");
+  return 0;
+}
